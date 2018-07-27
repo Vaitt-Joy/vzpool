0cc6c285dbab01d2e413ad0938eeb68de5cfce2e
vic <vaitt_joy@163.com>
2018-07-28 02:35:20 +0800

    add utils

--- src/common/commutils.go
+++ src/common/commutils.go
@@ -1,6 +1,19 @@
 package common
 
-import "strconv"
+import (
+	"strconv"
+	"math/rand"
+	"time"
+	"crypto/md5"
+	"encoding/hex"
+	"os"
+	"bufio"
+	"regexp"
+	"strings"
+	"net/smtp"
+	"io"
+	"crypto/sha1"
+)
 
 func toInt(val string) int {
 	i, err := strconv.Atoi(val)
@@ -84,5 +97,267 @@ func toFloat64(val string) float64 {
 }
 
 func toBool(val string) bool {
-
+	b, err := strconv.ParseBool(val)
+	if err != nil {
+		return false
+	}
+	return b
 }
+
+// 生成一个随机数
+
+func RandonInt(start, end int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(end-start) + start
+}
+
+
+func MD5(s string) string {
+	hash := md5.New()
+	hash.Write([]byte(s))
+	result := hex.EncodeToString(hash.Sum(nil))
+	return result
+}
+
+// 对字符串进行md5哈希,
+// 返回16位小写md5结果
+func MD5_16(s string) string {
+	return MD5(s)[8:24]
+}
+
+/* 文件的md5 值*/
+func Md5File(path string) string {
+	file, err := os.Open(path)
+	defer file.Close()
+	md5 := ""
+
+	if err != nil {
+		return ""
+	}
+
+	data := make([]byte, 1024)
+	for {
+		n, err := file.Read(data)
+
+		if n != 0 {
+			md5 = MD5(string(data))
+		} else {
+			break
+		}
+
+		if err != nil && err != io.EOF {
+			return ""
+		}
+	}
+
+	return md5
+}
+
+// 对字符串进行sha1哈希,
+// 返回42位小写sha1结果
+func SHA1(s string) string {
+
+	hasher := sha1.New()
+	hasher.Write([]byte(s))
+
+	//result := fmt.Sprintf("%x", (hasher.Sum(nil)))
+	result := hex.EncodeToString(hasher.Sum(nil))
+	return result
+}
+
+func HashFile(path string) string {
+	file, err := os.Open(path)
+	defer file.Close()
+	hash := ""
+
+	if err != nil {
+		return ""
+	}
+
+	data := make([]byte, 1024)
+	for {
+		n, err := file.Read(data)
+
+		if n != 0 {
+			//hash = MD5(string(data))
+			hash = SHA1(string(data))
+		} else {
+			break
+		}
+
+		if err != nil && err != io.EOF {
+			//panic(err)
+			return ""
+		}
+	}
+
+	return hash
+}
+
+func Writefile(path string, filename string, content string) error {
+	//path = path[0 : len(path)-len(filename)]
+	filename = path + filename
+	os.MkdirAll(path, 0644)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	} else {
+		writer := bufio.NewWriter(file)
+		writer.WriteString(content)
+		writer.Flush()
+	}
+	defer file.Close()
+	return nil
+}
+
+func Exist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+/*4-16 位字母*/
+func CheckPassword(password string) (b bool) {
+	if ok, _ := regexp.MatchString("^[a-zA-Z0-9]{4,16}$", password); !ok {
+		return false
+	}
+	return true
+}
+/*4-16 位字母*/
+func CheckUsername(username string) (b bool) {
+	if ok, _ := regexp.MatchString("^[a-zA-Z0-9]{4,16}$", username); !ok {
+		return false
+	}
+	return true
+}
+
+func CheckEmail(email string) (b bool) {
+	if ok, _ := regexp.MatchString(`^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$`, email); !ok {
+		return false
+	}
+	return true
+}
+
+/**
+* user : example@example.com login smtp server user
+* password: xxxxx login smtp server password
+* host: smtp.example.com:port   smtp.163.com:25
+* to: example@example.com;example1@163.com;example2@sina.com.cn;...
+* subject:The subject of mail
+* body: The content of mail
+* mailtype: mail type html or text
+ */
+func SendMail(user, password, host, to, subject, body, mailtype string) error {
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	var content_type string
+	if mailtype == "html" {
+		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+	msg := []byte("To: " + to + "\r\nFrom: " + user + "<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+	send_to := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, send_to, msg)
+	return err
+}
+
+
+func Html2str(html string) string {
+	src := string(html)
+
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	return strings.TrimSpace(src)
+}
+
+//截取字符
+func Substr(str string, start, length int, symbol string) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+
+	if start > end {
+		start, end = end, start
+	}
+
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+
+	return string(rs[start:end]) + symbol
+}
+
+func Htmlquote(text string) string {
+	//HTML编码为实体符号
+	/*
+	   Encodes `text` for raw use in HTML.
+	       >>> htmlquote("<'&\\">")
+	       '&lt;&#39;&amp;&quot;&gt;'
+	*/
+
+	text = strings.Replace(text, "&", "&amp;", -1) // Must be done first!
+	text = strings.Replace(text, "<", "&lt;", -1)
+	text = strings.Replace(text, ">", "&gt;", -1)
+	text = strings.Replace(text, "'", "&#39;", -1)
+	text = strings.Replace(text, "\"", "&quot;", -1)
+	text = strings.Replace(text, "“", "&ldquo;", -1)
+	text = strings.Replace(text, "”", "&rdquo;", -1)
+	text = strings.Replace(text, " ", "&nbsp;", -1)
+	return text
+}
+
+func Htmlunquote(text string) string {
+	//实体符号解释为HTML
+	/*
+	   Decodes `text` that's HTML quoted.
+	       >>> htmlunquote('&lt;&#39;&amp;&quot;&gt;')
+	       '<\\'&">'
+	*/
+
+	// strings.Replace(s, old, new, n)
+	// 在s字符串中，把old字符串替换为new字符串，n表示替换的次数，小于0表示全部替换
+
+	text = strings.Replace(text, "&nbsp;", " ", -1)
+	text = strings.Replace(text, "&rdquo;", "”", -1)
+	text = strings.Replace(text, "&ldquo;", "“", -1)
+	text = strings.Replace(text, "&quot;", "\"", -1)
+	text = strings.Replace(text, "&#39;", "'", -1)
+	text = strings.Replace(text, "&gt;", ">", -1)
+	text = strings.Replace(text, "&lt;", "<", -1)
+	text = strings.Replace(text, "&amp;", "&", -1) // Must be done last!
+	return text
+}
+
+
